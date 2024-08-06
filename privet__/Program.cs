01836b66fcfa09e2db696e797f3d5ac3a5e96498
menachem <AM0556727820@GMAIL.COM>
2024-08-06 09:31:59 +0300

    Add project files.

---
+++ privet__/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+Person person = new Person();
+person.SetAge(4); 
+Console.WriteLine($"age= {person.GetAge}");
+
+
+public class Person
+{
+
+    public Person() { }
+    private int Age = 0;
+    public void SetAge(int Age)
+    {
+        this.Age = Age;
+    }
+    public int GetAge { get { return this.Age; } }
+}
+
+
